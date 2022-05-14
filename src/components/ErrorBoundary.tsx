823e5be1899086dfc79caae458d6c00deb959b58
Olie Chan <olie.chan.141@gmail.com>
2022-05-14 17:15:32 +0100

    add error boundary component

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_error: any) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <View style={styles.container}>
+          <Text>Something went wrong</Text>
+        </View>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
