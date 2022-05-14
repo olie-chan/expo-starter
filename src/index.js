da270864f751f6afad1b627051edc183811e2188
Olie Chan <olie.chan.141@gmail.com>
2022-05-14 12:47:49 +0100

    edit entry point

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
+import { HelloWorld } from "./components/HelloWorld";
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HelloWorld />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
